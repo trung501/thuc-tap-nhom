ae56066cc04b3171d3c4d3ab47b457f795d625be
trung501 <gotrungogle@gmail.com>
2022-11-06 02:36:39 +0700

    tao template dau tien

---
+++ GUI/Form1.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraBars;
+using GUI.Properties;
+using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : DevExpress.XtraBars.FluentDesignSystem.FluentDesignForm
+    {
+       
+        public Form1(string maTK, string pass) //uyen
+        {
+            InitializeComponent();
+           
+            
+        }
+       
+    }
+}
