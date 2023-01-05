d553c1929c121efb0f835124362a2e7195263308
Khương Duy <70515235+khuongduyktqs@users.noreply.github.com>
2023-01-05 19:41:17 +0700

    QLKS-47 Kết nối CSDL

--- HotelManager/DAO/DataProvider.cs
+++ HotelManager/DAO/DataProvider.cs
@@ -11,6 +11,25 @@ namespace HotelManager.DAO
     public class DataProvider
     {
         private static DataProvider instance;
-       
-    }
+        private string connectionStr = @"Data Source=TRUNG\TRUNG;Initial Catalog=HotelManagement;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+          //private string connectionStr = @"Data Source=THIEN-AI\THIENAI;Initial Catalog=HotelManagement;Integrated Security=True";
+          //private string connectionStr = @"Data Source=.\sqlexpress;Initial Catalog=HotelManagement;Integrated Security=True";
+          public DataTable ExecuteQuery(string query, object[] parameter = null)
+          {
+               DataTable data = new DataTable();
+               using (SqlConnection connection = new SqlConnection(connectionStr))
+               {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    AddParameter(query, parameter, command);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(data);
+                    connection.Close();
+               }
+               return data;
+          }
+
+          private DataProvider() { }
+     }
 }
