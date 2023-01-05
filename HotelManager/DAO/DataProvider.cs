783ac5428b3ffa7df7a91b3dde8d259b8c2e72c8
Khương Duy <70515235+khuongduyktqs@users.noreply.github.com>
2023-01-05 19:43:47 +0700

    QLKS-47 Tiếp tục

--- HotelManager/DAO/DataProvider.cs
+++ HotelManager/DAO/DataProvider.cs
@@ -29,7 +29,53 @@ namespace HotelManager.DAO
                }
                return data;
           }
-
+          public int ExecuteNoneQuery(string query, object[] parameter = null)
+          {
+               int data = 0;
+               using (SqlConnection connection = new SqlConnection(connectionStr))
+               {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    AddParameter(query, parameter, command);
+                    data = command.ExecuteNonQuery();
+                    connection.Close();
+               }
+               return data;
+          }
+          public object ExecuteScalar(string query, object[] parameter = null)
+          {
+               object data = new object();
+               using (SqlConnection connection = new SqlConnection(connectionStr))
+               {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    AddParameter(query, parameter, command);
+                    data = command.ExecuteScalar();
+                    connection.Close();
+               }
+               return data;
+          }
+          private void AddParameter(string query, object[] parameter, SqlCommand command)
+          {
+               if (parameter != null)
+               {
+                    string[] listParameter = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listParameter)
+                    {
+                         if (item.Contains("@"))
+                         {
+                              command.Parameters.AddWithValue(item, parameter[i]);
+                              ++i;
+                         }
+                    }
+               }
+          }
+          public static DataProvider Instance
+          {
+               get { if (instance == null) instance = new DataProvider(); return instance; }
+               private set => instance = value;
+          }
           private DataProvider() { }
      }
 }
