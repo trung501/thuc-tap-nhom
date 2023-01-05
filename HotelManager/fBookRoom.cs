2b94f55e7813d3d0dcdbd38d567d125554e99f8a
ATT21072001 <anthanhtulkt@gmail.com>
2023-01-05 19:36:05 +0700

    QLKS-52 Finish task

--- HotelManager/fBookRoom.cs
+++ HotelManager/fBookRoom.cs
@@ -15,6 +15,128 @@ namespace HotelManager
 {
     public partial class fBookRoom : Form
     {
+        public fBookRoom()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+        public void LoadData()
+        {
+            LoadRoomType();
+            LoadCustomerType();
+            LoadDate();
+            LoadDays();
+            LoadListBookRoom();
+        }
+        public void LoadRoomType()
+        {
+            cbRoomType.DataSource = RoomTypeDAO.Instance.LoadListRoomType();
+            cbRoomType.DisplayMember = "Name";
+        }
+        public void LoadRoomTypeInfo(int id)
+        {
+            RoomType roomType = RoomTypeDAO.Instance.LoadRoomTypeInfo(id);
+            txbRoomTypeID.Text = roomType.Id.ToString();
+            txbRoomTypeName.Text = roomType.Name;
+            CultureInfo cultureInfo = new CultureInfo("vi-vn");
+            txbPrice.Text = roomType.Price.ToString("c0", cultureInfo);
+            txbAmountPeople.Text = roomType.LimitPerson.ToString();
+        }
+        public void LoadDate()
+        {
+            dpkDateOfBirth.Value = new DateTime(1998, 4, 6);
+            dpkDateCheckIn.Value = DateTime.Now;
+            dpkDateCheckOut.Value = DateTime.Now.AddDays(1);
+        }
+        public void LoadDays()
+        {
+            txbDays.Text = (dpkDateCheckOut.Value.Date - dpkDateCheckIn.Value.Date).Days.ToString();
+        }
+        public void LoadCustomerType()
+        {
+            cbCustomerType.DataSource = CustomerTypeDAO.Instance.LoadListCustomerType();
+            cbCustomerType.DisplayMember = "Name";
+        }
+        public bool IsIdCardExists(string idCard)
+        {
+            return CustomerDAO.Instance.IsIdCardExists(idCard);
+        }
+        public void InsertCustomer(string idCard, string name, int idCustomerType, DateTime dateofBirth, string address, int phonenumber, string sex, string nationality)
+        {
+            CustomerDAO.Instance.InsertCustomer(idCard, name, idCustomerType, dateofBirth, address, phonenumber, sex, nationality);
+        }
+        public void GetInfoByIdCard(string idCard)
+        {
+            Customer customer = CustomerDAO.Instance.GetInfoByIdCard(idCard);
+            txbIDCard.Text = customer.IdCard.ToString();
+            txbFullName.Text = customer.Name;
+            txbAddress.Text = customer.Address;
+            dpkDateOfBirth.Value = customer.DateOfBirth;
+            cbSex.Text = customer.Sex;
+            txbPhoneNumber.Text = customer.PhoneNumber.ToString();
+            cbNationality.Text = customer.Nationality;
+            cbCustomerType.Text = CustomerTypeDAO.Instance.GetNameByIdCard(idCard);
+        }
+        public void InsertBookRoom(int idCustomer, int idRoomType, DateTime datecheckin, DateTime datecheckout, DateTime datebookroom)
+        {
+            BookRoomDAO.Instance.InsertBookRoom(idCustomer, idRoomType, datecheckin, datecheckout, datebookroom);
+        }
+        public int GetCurrentIDBookRoom(DateTime dateTime)
+        {
+            return BookRoomDAO.Instance.GetCurrentIDBookRoom(dateTime);
+        }
+        public void LoadListBookRoom()
+        {
+            dataGridViewBookRoom.DataSource = BookRoomDAO.Instance.LoadListBookRoom(DateTime.Now.Date);
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txbDays_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void cbRoomType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadRoomTypeInfo((cbRoomType.SelectedItem as RoomType).Id);
+        }
+
+        private void dpkDateCheckOut_onValueChanged(object sender, EventArgs e)
+        {
+            if (dpkDateCheckOut.Value < DateTime.Now)
+                LoadDate();
+            if (dpkDateCheckOut.Value <= dpkDateCheckIn.Value)
+                LoadDate();
+            LoadDays();
+        }
+
+        private void dpkDateCheckIn_onValueChanged(object sender, EventArgs e)
+        {
+            if (dpkDateCheckIn.Value <= DateTime.Now)
+                LoadDate();
+            if (dpkDateCheckOut.Value <= dpkDateCheckIn.Value)
+                LoadDate();
+            LoadDays();
+        }
+
+        private void txbIDCardSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+                e.Handled = true;
+            if (e.KeyChar == 13)
+                btnSearch_Click(sender, null);
+        }
+
+        private void txbIDCard_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+
         private void btnSearch_Click(object sender, EventArgs e)
         {
             if (txbIDCardSearch.Text != String.Empty)
@@ -25,7 +147,11 @@ namespace HotelManager
                     MessageBox.Show("Thẻ căn cước/ CMND không tồn tại.\nVui lòng nhập lại.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-
+        public void ClearData()
+        {
+            txbIDCardSearch.Text = txbIDCard.Text = txbFullName.Text = txbAddress.Text = txbPhoneNumber.Text = cbNationality.Text = String.Empty;
+            LoadDate();
+        }
         private void btnBookRoom_Click(object sender, EventArgs e)
         {
             if (MessageBox.Show("Bạn có muốn đặt phòng không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
@@ -71,6 +197,12 @@ namespace HotelManager
             f.ShowDialog();
             Show();
             LoadListBookRoom();
+        }
+
+        private void txbPhoneNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+                e.Handled = true;
         }
     }
 }
