32c9851b8140583c7e99a94ea15e19cd67bc0845
HNN510 <honguyen54321@gmail.com>
2023-01-06 15:24:25 +0700

    QLKS-69 thống kê doanh thu 1

--- HotelManager/fReport.cs
+++ HotelManager/fReport.cs
@@ -9,6 +9,28 @@ namespace HotelManager
 {
     public partial class fReport : Form
     {
-       
+        private int month = 1;
+        private int year = 1990;
+        public fReport()
+        {
+            InitializeComponent();
+            dataGridReport.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+        }
+
+        #region Load
+        private void LoadFullReport(int month, int year)
+        {
+            this.month = month;
+            this.year = year;
+            DataTable table = GetFulReport(month, year);
+            BindingSource source = new BindingSource();
+            //ChangePrice(table);
+            source.DataSource = table;
+            dataGridReport.DataSource = source;
+            bindingReport.BindingSource = source;
+            DrawChart(source);
+            GC.Collect();
+        }
+        #endregion
     }
 }
