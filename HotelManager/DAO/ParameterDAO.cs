7699fbf15c7af62f8b6d5ae8af7140a0a5637b81
UYEN <annguyenan@gmail.com>
2023-01-06 21:44:04 +0700

    QLKS-68 load_full

--- HotelManager/DAO/ParameterDAO.cs
+++ HotelManager/DAO/ParameterDAO.cs
@@ -6,7 +6,19 @@ namespace HotelManager.DAO
 {
     public class ParameterDAO
     {
-       
+        #region Properties && constructor
+        private ParameterDAO() { }
+
+        private static ParameterDAO instance;
+        internal static ParameterDAO Instance { get { if (instance == null) instance = new ParameterDAO(); return instance; } }
+        #endregion
+
+        #region Method
+        internal DataTable LoadFullParameter()
+        {
+            return DataProvider.Instance.ExecuteQuery("USP_LoadFullParameter");
+        }
+        #endregion
 
     }
 }
