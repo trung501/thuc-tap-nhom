7e1b10b941988b896b34714c389082c84a756f5e
UYEN <annguyenan@gmail.com>
2023-01-06 22:46:16 +0700

    QLKS-68 update

--- HotelManager/DAO/ParameterDAO.cs
+++ HotelManager/DAO/ParameterDAO.cs
@@ -14,6 +14,15 @@ namespace HotelManager.DAO
         #endregion
 
         #region Method
+        internal bool UpdateParameter(string name, double value, string describe)
+        {
+            string query = "exec USP_UpdateParameter @name , @value , @describe";
+            return DataProvider.Instance.ExecuteNoneQuery(query, new Object[] { name, value, describe }) > 0;
+        }
+        internal bool UpdateParameter(Parameter surcharge)
+        {
+            return UpdateParameter(surcharge.Name, surcharge.Value, surcharge.Describe);
+        }
         internal DataTable LoadFullParameter()
         {
             return DataProvider.Instance.ExecuteQuery("USP_LoadFullParameter");
