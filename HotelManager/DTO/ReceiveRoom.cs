6431cba4ba4d8a8c1c11a9b8ee97e16ae95d6360
HNN510 <honguyen54321@gmail.com>
2023-01-06 10:13:26 +0700

    QLKS-62 hoàn thành DTO nhận phòng

--- HotelManager/DTO/ReceiveRoom.cs
+++ HotelManager/DTO/ReceiveRoom.cs
@@ -9,6 +9,17 @@ namespace HotelManager.DTO
 {
     public class ReceiveRoom
     {
-       
+        private int id;
+        private int idBookRoom;
+        private int idRoom;
+        public ReceiveRoom(DataRow row)
+        {
+            Id = (int)row["id"];
+            IdBookRoom = (int)row["idBookRoom"];
+            IdRoom = (int)row["idroom"];
+        }
+        public int Id { get => id; set => id = value; }
+        public int IdBookRoom { get => idBookRoom; set => idBookRoom = value; }
+        public int IdRoom { get => idRoom; set => idRoom = value; }
     }
 }
