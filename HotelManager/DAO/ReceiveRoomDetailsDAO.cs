002944f46d850528ce136f392394b209acddacf7
HNN510 <honguyen54321@gmail.com>
2023-01-07 08:34:02 +0700

    QLKS-105 Viết DAO nhận phòng chi tiết

--- HotelManager/DAO/ReceiveRoomDetailsDAO.cs
+++ HotelManager/DAO/ReceiveRoomDetailsDAO.cs
@@ -8,6 +8,24 @@ namespace HotelManager.DAO
 {
     public class ReceiveRoomDetailsDAO
     {
-        
+        private static ReceiveRoomDetailsDAO instance;
+        private ReceiveRoomDetailsDAO() { }
+        public bool InsertReceiveRoomDetails(int idReceiveRoom, int idCustomerOther)
+        {
+            string query = "InsertReceiveRoomDetails @idReceiveRoom , @idCustomer";
+            int count = DataProvider.Instance.ExecuteNoneQuery(query, new object[] { idReceiveRoom, idCustomerOther });
+            return count > 0;
+        }
+        public bool DeleteReceiveRoomDetails(int idReceiveRoom, int idCustomerOther)
+        {
+            string query = "USP_DeleteReceiveRoomDetails @idReceiveRoom , @idCustomer";
+            int count = DataProvider.Instance.ExecuteNoneQuery(query, new object[] { idReceiveRoom, idCustomerOther });
+            return count > 0;
+        }
+        public static ReceiveRoomDetailsDAO Instance
+        {
+            get { if (instance == null) instance = new ReceiveRoomDetailsDAO(); return instance; }
+            private set => instance = value;
+        }
     }
 }
