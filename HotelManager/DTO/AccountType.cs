97c487ee8d8bb8587a9647df6e013e21ba435417
Khương Duy <70515235+khuongduyktqs@users.noreply.github.com>
2023-01-07 20:00:06 +0700

    QLKS-111-xay-dựng-dto-thong-tin-ca-nhan

--- HotelManager/DTO/AccountType.cs
+++ HotelManager/DTO/AccountType.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace HotelManager.DTO
 {
-    public class AccountType
-    {
-        
-    }
+     public class AccountType
+     {
+          private int id;
+          private string name;
+          public AccountType(DataRow dataRow)
+          {
+               Id = (int)dataRow["id"];
+               Name = dataRow["name"].ToString();
+          }
+
+          public int Id { get => id; set => id = value; }
+          public string Name { get => name; set => name = value; }
+     }
 }
