73915d4298971e8aa53508bb2356666eae8ff98a
ATT21072001 <anthanhtulkt@gmail.com>
2023-01-08 20:37:57 +0700

    QLKS-132 finish button

--- HotelManager/fCustomer.cs
+++ HotelManager/fCustomer.cs
@@ -8,7 +8,24 @@ namespace HotelManager
 {
     public partial class fCustomer : Form
     {
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
 
-       
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddCustomer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
