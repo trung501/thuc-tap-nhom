ecd7fde95ae41ac7492453286e59b610e832803e
UYEN <annguyenan@gmail.com>
2023-01-08 12:24:43 +0700

    QLKS-100 get access

--- HotelManager/DAO/AccessDAO.cs
+++ HotelManager/DAO/AccessDAO.cs
@@ -9,6 +9,22 @@ namespace HotelManager.DAO
 {
     class AccessDAO
     {
-       
+        #region Properties
+        private static AccessDAO instance = new AccessDAO();
+        internal static AccessDAO Instance { get => instance; }
+        private AccessDAO() { }
+        #endregion
+
+        public DataTable GetFullAccessNow(int idStaffType)
+        {
+            string query = "USP_LoadFullAccessNow @idStaffType";
+            return DataProvider.Instance.ExecuteQuery(query, new object[] { idStaffType });
+        }
+        public DataTable GetFullAccessRest(int idStaffType)
+        {
+            string query = "USP_LoadFullAccessRest @idStaffType";
+            return DataProvider.Instance.ExecuteQuery(query, new object[] { idStaffType });
+        }
+
     }
 }
