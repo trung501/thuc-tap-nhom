72bdde0e1744dba6788f067284706ee47a4432f0
HNN510 <honguyen54321@gmail.com>
2023-01-09 14:41:22 +0700

    QLKS-114 Thiết kế giao diện quản lý phòng 2

--- HotelManager/fRoom.cs
+++ HotelManager/fRoom.cs
@@ -100,5 +100,104 @@ namespace HotelManager
         }
 
         #endregion
+
+        #region Load
+        private void LoadFullRoom(DataTable table)
+        {
+            BindingSource source = new BindingSource();
+            ChangePrice(table);
+            source.DataSource = table;
+            dataGridViewRoom.DataSource = source;
+            bindingRoom.BindingSource = source;
+            comboboxID.DataSource = source;
+        }
+        private void LoadFullStatusRoom()
+        {
+            DataTable table = GetFullStatusRoom();
+            comboBoxStatusRoom.DataSource = table;
+            comboBoxStatusRoom.DisplayMember = "Name";
+            if (table.Rows.Count > 0)
+                comboBoxStatusRoom.SelectedIndex = 0;
+        }
+        private void LoadFullRoomType()
+        {
+            DataTable table = GetFullRoomType();
+            comboBoxRoomType.DataSource = table;
+            comboBoxRoomType.DisplayMember = "Name";
+            if (table.Rows.Count > 0)
+                comboBoxRoomType.SelectedIndex = 0;
+            _fRoomtType = new fRoomType(table);
+            txbLimitPerson.DataBindings.Add(new Binding("Text", comboBoxRoomType.DataSource, "limitPerson"));
+        }
+        #endregion
+
+        #region Method
+
+        private void UpdateRoom()
+        {
+            if (comboboxID.Text == string.Empty)
+            {
+                MessageBox.Show("Phòng này chưa tồn tại\n", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else
+            if (!fCustomer.CheckFillInText(new Control[] { txbNameRoom, comboBoxStatusRoom, comboBoxRoomType }))
+            {
+                MessageBox.Show("Không được để trống", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                Room roomPre = groupRoom.Tag as Room;
+                try
+                {
+                    Room roomNow = GetRoomNow();
+                    if (roomNow.Equals(roomPre))
+                    {
+                        MessageBox.Show("Bạn chưa thay đổi dữ liệu", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        bool check = RoomDAO.Instance.UpdateCustomer(roomNow);
+                        if (check)
+                        {
+                            MessageBox.Show("Cập Nhật Thành Công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            groupRoom.Tag = roomNow;
+                            if (btnCancel.Visible == false)
+                            {
+                                int index = dataGridViewRoom.SelectedRows[0].Index;
+                                LoadFullRoom(GetFullRoom());
+                                comboboxID.SelectedIndex = index;
+                            }
+                            else BtnCancel_Click(null, null);
+                        }
+                        else
+                            MessageBox.Show("Phòng này chưa tồn tại\n", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Lỗi cập nhật phòng này", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void ChangeText(DataGridViewRow row)
+        {
+            if (row.IsNewRow)
+            {
+                txbNameRoom.Text = string.Empty;
+                bindingNavigatorMoveFirstItem.Enabled = false;
+                bindingNavigatorMovePreviousItem.Enabled = false;
+            }
+            else
+            {
+                bindingNavigatorMoveFirstItem.Enabled = true;
+                bindingNavigatorMovePreviousItem.Enabled = true;
+                txbNameRoom.Text = row.Cells["colName"].Value.ToString();
+                comboBoxRoomType.SelectedIndex = (int)row.Cells["colIdRoomType"].Value - 1;
+                comboBoxStatusRoom.SelectedIndex = (int)row.Cells["colIdStatus"].Value - 1;
+                Room room = new Room(((DataRowView)row.DataBoundItem).Row);
+                groupRoom.Tag = room;
+            }
+        }
     }
 }
