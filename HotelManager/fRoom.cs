0ebfd5e5ee086340c4ac28f5653d76520bb45a71
HNN510 <honguyen54321@gmail.com>
2023-01-09 14:40:19 +0700

    QLKS-114 Thiết kế giao diện quản lý phòng 1

--- HotelManager/fRoom.cs
+++ HotelManager/fRoom.cs
@@ -9,6 +9,96 @@ namespace HotelManager
 {
     public partial class fRoom : Form
     {
-       
+        #region Properties
+        private fRoomType _fRoomtType;
+        #endregion
+
+        #region Constructor
+        public fRoom()
+        {
+            InitializeComponent();
+            LoadFullRoomType();
+            LoadFullStatusRoom();
+            LoadFullRoom(GetFullRoom());
+            dataGridViewRoom.SelectionChanged += DataGridViewRoom_SelectionChanged;
+            comboboxID.DisplayMember = "id";
+            dataGridViewRoom.ColumnHeadersDefaultCellStyle.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+        }
+
+        #endregion
+
+        #region Kick
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void BtnAddRoom_Click(object sender, EventArgs e)
+        {
+            new fAddRoom().ShowDialog();
+            if (btnCancel.Visible == false)
+                LoadFullRoom(GetFullRoom());
+            else BtnCancel_Click(null, null);
+        }
+        private void BtnRoomType_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            _fRoomtType.ShowDialog();
+            LoadFullRoom(GetFullRoom());
+            comboBoxRoomType.DataSource = _fRoomtType.TableRoomType;
+            txbPrice.DataBindings.Clear();
+            txbLimitPerson.DataBindings.Clear();
+            txbPrice.DataBindings.Add(new Binding("Text", comboBoxRoomType.DataSource, "price_new"));
+            txbLimitPerson.DataBindings.Add(new Binding("Text", comboBoxRoomType.DataSource, "limitPerson"));
+            this.Show();
+        }
+        private void BtnCLose1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void BindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            txbNameRoom.Text = string.Empty;
+        }
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có muốn cập nhật lại phòng?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+            if (result == DialogResult.OK)
+                UpdateRoom();
+            comboboxID.Focus();
+        }
+        private void ToolStripLabel1_Click(object sender, EventArgs e)
+        {
+            if (saveRoom.ShowDialog() == DialogResult.Cancel)
+                return;
+            else
+            {
+                bool check;
+                try
+                {
+                    switch (saveRoom.FilterIndex)
+                    {
+                        case 2:
+                            check = ExportToExcel.Instance.Export(dataGridViewRoom, saveRoom.FileName, ModeExportToExcel.XLSX);
+                            break;
+                        case 3:
+                            check = ExportToExcel.Instance.Export(dataGridViewRoom, saveRoom.FileName, ModeExportToExcel.PDF);
+                            break;
+                        default:
+                            check = ExportToExcel.Instance.Export(dataGridViewRoom, saveRoom.FileName, ModeExportToExcel.XLS);
+                            break;
+                    }
+                    if (check)
+                        MessageBox.Show("Xuất thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    else
+                        MessageBox.Show("Lỗi xuất thất bại", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch
+                {
+                    MessageBox.Show("Lỗi (Cần cài đặt Office)", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        #endregion
     }
 }
